989e878420497e593485ae44adc5e3771797df51
Tim <tim@ubergrund.de>
2013-10-29 07:15:06 +0100

    pumped up the method count

--- src/com/ubergrund/android/tinyjournal/JournalSyncAdapter.java
+++ src/com/ubergrund/android/tinyjournal/JournalSyncAdapter.java
@@ -10,7 +10,9 @@ import android.provider.CallLog;
 import android.provider.ContactsContract;
 import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -27,19 +29,18 @@ public class JournalSyncAdapter extends AbstractThreadedSyncAdapter {
     private static final Uri SMS_CONTENT_URI = Uri.parse("content://sms");
 //    SMS columns: _id,thread_id,address,person,date,date_sent,protocol,read,status,type,reply_path_present,subject,body,service_center,locked,error_code,seen
 
+    private final Map<String,Long> phoneNumber2ContactIdCache = new HashMap<String, Long>();
+
     private final ContentResolver resolver;
-    private final ContactCache contactCache;
 
     public JournalSyncAdapter(Context context, boolean autoInitialize) {
         super(context, autoInitialize);
         resolver = context.getContentResolver();
-        contactCache = new ContactCache(resolver);
     }
 
     public JournalSyncAdapter(Context context, boolean autoInitialize, boolean allowParallelSyncs) {
         super(context, autoInitialize, allowParallelSyncs);
         resolver = context.getContentResolver();
-        contactCache = new ContactCache(resolver);
     }
 
     @Override
@@ -49,9 +50,18 @@ public class JournalSyncAdapter extends AbstractThreadedSyncAdapter {
         syncCallLog();
         syncSMSHistory();
 
+        syncJournalContentProviderToSocialStream();
+
     }
 
+    /**
+     * This method parses the system's call log ContentProvider and syncs
+     * the entries with the JournalContentProvider
+     */
     private void syncCallLog() {
+
+        // holds all known entry ids of the system's call log to delete entries in JournalProvider missing in source
+        final List<Long> systemCallLogIds = new ArrayList<Long>();
 
         long latestId = getLatestId(KEY_LATEST_CALLLOG_ID);
         final long lastLatestId = latestId;
@@ -82,11 +92,14 @@ public class JournalSyncAdapter extends AbstractThreadedSyncAdapter {
                 final int type = callLogQuery.getInt(colType);
                 callLogQuery.moveToNext();
 
+                systemCallLogIds.add(id);
                 latestId = id;
 
-                final long contactId = contactCache.getContactId(number);
+                final long contactId = getRawContactId(number);
                 Log.d(TAG, id + ": " + date+" ("+type+") " + number + " ["+contactId+"]"  + " ("+duration+")");
 
+                // get own raw contact id from contactId
+                // sync data with JournalContentProvider
             }
         } finally {
             callLogQuery.close();
@@ -96,7 +109,15 @@ public class JournalSyncAdapter extends AbstractThreadedSyncAdapter {
         }
     }
 
+    /**
+     * This method parses the system's SMS ContentProvider and syncs
+     * the entries with the JournalContentProvider
+     */
     private void syncSMSHistory() {
+
+        // holds all known entry ids of the system's SMS table to delete entries in JournalProvider missing in source
+        final List<Long> systemSMSIds = new ArrayList<Long>();
+
         long latestId = getLatestId(KEY_LATEST_SMS_ID);
         final long lastLatestId = latestId;
 
@@ -132,12 +153,12 @@ public class JournalSyncAdapter extends AbstractThreadedSyncAdapter {
                 final int type = smsQuery.getInt(colType);
                 final String subject = smsQuery.getString(colSubject);
                 final String body = smsQuery.getString(colBody);
-
                 smsQuery.moveToNext();
 
+                systemSMSIds.add(id);
                 latestId = id;
 
-                final long contactId = contactCache.getContactId(address);
+                final long contactId = getRawContactId(address);
                 Log.d(TAG, id + ": " + date+" ("+type+") " + address + " ["+contactId+"]"  + " ("+subject+")");
 
             }
@@ -147,6 +168,16 @@ public class JournalSyncAdapter extends AbstractThreadedSyncAdapter {
                 saveLatestId(KEY_LATEST_SMS_ID, latestId);
             }
         }
+    }
+
+    /**
+     * This method adds new entries from JournalContentProvider to
+     * the social streams ContentProvider of Contacts
+     * It also removes / updates entries in social streams different to
+     * those stored in JournalContentProvider.
+     */
+    private void syncJournalContentProviderToSocialStream() {
+
     }
 
     private void saveLatestId(String key, long id) {
@@ -161,41 +192,46 @@ public class JournalSyncAdapter extends AbstractThreadedSyncAdapter {
         return preferences.getLong(key, -1);
     }
 
-    private class ContactCache {
+    /**
+     * This method resolves a phone number to a raw contact id of TinyJournal.
+     * If the raw contact does not yet exist, it will be created.
+     *
+     * @param phoneNumber
+     * @return
+     */
+    public long getRawContactId(String phoneNumber) {
+        final long contactId = phoneNumber2ContactId(phoneNumber);
+        // TODO get raw contact id of type journal for contactId
+        // TODO if not exists, create
 
-        private final Map<String,Long> phoneNumber2ContactIdCache = new HashMap<String, Long>();
-        private final ContentResolver resolver;
+        return contactId;
+    }
 
-        private ContactCache(ContentResolver resolver) {
-            this.resolver = resolver;
+    public long phoneNumber2ContactId(String phoneNumber) {
+        if (phoneNumber2ContactIdCache.containsKey(phoneNumber))
+            return phoneNumber2ContactIdCache.get(phoneNumber);
+
+        long id = -1;
+        Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
+        final Cursor contactQuery = resolver.query(uri, new String[]{ContactsContract.PhoneLookup._ID, ContactsContract.PhoneLookup.DISPLAY_NAME}, null, null, null);
+        if (contactQuery == null) {
+            Log.w(TAG, "Unable to resolve number, query returned null");
+            return id;
         }
 
-        public long getContactId(String phoneNumber) {
-            if (phoneNumber2ContactIdCache.containsKey(phoneNumber))
-                return phoneNumber2ContactIdCache.get(phoneNumber);
-
-            Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
-            final Cursor contactQuery = resolver.query(uri, new String[]{ContactsContract.PhoneLookup._ID, ContactsContract.PhoneLookup.DISPLAY_NAME}, null, null, null);
-            if (contactQuery == null) {
-                Log.w(TAG, "Unable to resolve number, query returned null");
-                return -1;
-            }
-
-            try {
-                if (!contactQuery.moveToFirst()) {
-                    Log.w(TAG, "Empty result");
-                    phoneNumber2ContactIdCache.put(phoneNumber, -1l);
-                    return -1;
-                }
-                final long id = contactQuery.getLong(0);
-                final String name = contactQuery.getString(1);
-                phoneNumber2ContactIdCache.put(phoneNumber, id);
-                Log.d(TAG, "Resolved ["+phoneNumber+"] to ["+name+"]");
+        try {
+            if (!contactQuery.moveToFirst()) {
+                Log.w(TAG, "Empty result");
                 return id;
             }
-            finally {
-                contactQuery.close();
-            }
+            id = contactQuery.getLong(0);
+            final String name = contactQuery.getString(1);
+            Log.d(TAG, "Resolved ["+phoneNumber+"] to ["+name+"]");
+            return id;
+        }
+        finally {
+            contactQuery.close();
+            phoneNumber2ContactIdCache.put(phoneNumber, id);
         }
     }
 
